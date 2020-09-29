b5f3285c0d3c96615eb16cb6616eb58473a28294
florentcalabresi <60184656+florentcalabresi@users.noreply.github.com>
2020-09-29 16:47:52 +0200

    Updating source code plugin PermiFlow

--- src/main/java/fr/northenflo/permiflow/Main.java
+++ src/main/java/fr/northenflo/permiflow/Main.java
@@ -1,9 +1,30 @@
 package fr.northenflo.permiflow;
 
+import org.bukkit.entity.Player;
+import org.bukkit.permissions.PermissionAttachment;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.UUID;
 
 public class Main extends JavaPlugin {
 
+    @Override
+    public void onEnable(){
+        HashMap<UUID, PermissionAttachment> perms = new HashMap<UUID, PermissionAttachment>();
+        Player player = this.getServer().getPlayer("NorthenFlo");
+        PermissionAttachment attachment = player.addAttachment(this);
+        perms.put(player.getUniqueId(), attachment);
 
+        PermissionAttachment pperms = perms.get(player.getUniqueId());
+        pperms.setPermission("permission.here", true);
+
+        perms.get(player.getUniqueId()).unsetPermission("permission.here");
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
 
 }
